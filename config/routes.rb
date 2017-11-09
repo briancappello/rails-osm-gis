d2ee443609b9982fa42b607818c5e3c51ea70c13
Brian Cappello <briancappello@gmail.com>
2017-11-09 13:57:33 -0500

    generate static pages boilerplate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   # router DSL docs: http://guides.rubyonrails.org/routing.html
 
   root 'application#hello'
