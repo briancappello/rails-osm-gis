9e33e60bc33c6bfe5e7b1601bf4d4f1c465cd415
Brian Cappello <briancappello@gmail.com>
2017-11-09 14:24:17 -0500

    update url structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # router DSL docs: http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
 end
