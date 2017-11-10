fd313faef1e8c71a024310ad6544f626c4155f3c
Brian Cappello <briancappello@gmail.com>
2017-11-10 09:55:37 -0500

    add test for valid user sign up

--- test/integration/users_sign_up_test.rb
+++ test/integration/users_sign_up_test.rb
@@ -1,15 +1,16 @@
 require 'test_helper'
 
 class UsersSignUpTest < ActionDispatch::IntegrationTest
+
   test 'should generate form errors' do
     get sign_up_path
     assert_no_difference 'User.count' do
-      post users_path, params: {
-          user: {
-            name: '',
-            email: 'a@invalid',
-            password: 'foo',
-            password_confirmation: 'bar'
+      post sign_up_path, params: {
+        user: {
+          name: '',
+          email: 'a@invalid',
+          password: 'foo',
+          password_confirmation: 'bar'
         }
       }
     end
@@ -17,4 +18,22 @@ class UsersSignUpTest < ActionDispatch::IntegrationTest
     assert_select 'div.form-errors'
     assert_select 'div.field_with_errors'
   end
+
+  test 'valid submission should persist a new user' do
+    get sign_up_path
+    assert_difference 'User.count', 1 do
+      post sign_up_path, params: {
+        user: {
+          name: 'name',
+          email: 'valid@a.com',
+          password: 'password',
+          password_confirmation: 'password'
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
 end
