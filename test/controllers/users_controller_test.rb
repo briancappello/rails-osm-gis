f7db66ab853345b68c95b57d89e70eab3c0f2a78
Brian Cappello <briancappello@gmail.com>
2017-11-09 22:42:32 -0500

    controller names should be plural in rails land

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UserControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should get new" do
     get sign_up_path
