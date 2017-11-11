b21d328a41acff7aad6621363a9853a9def24b12
Brian Cappello <briancappello@gmail.com>
2017-11-10 23:26:59 -0500

    remove unnecessary instance varialbe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
-    @count = @user.microposts.count()
   end
 
   def new
